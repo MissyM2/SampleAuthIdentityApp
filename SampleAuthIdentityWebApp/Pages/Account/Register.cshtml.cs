0cfa167f598db5360335618746df94dde2758242
maloney_f <missymaloney1@gmail.com>
2023-10-07 17:52:25 -0400

    reg page configured

---
+++ SampleAuthIdentityWebApp/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleAuthIdentityWebApp.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        [BindProperty]
+        public RegisterViewModel RegisterViewModel { get; set; } = new RegisterViewModel();
+
+        // we have already dependency injected Identity into the AspNetCore so we can grab an instance of it through the constructor
+        public RegisterModel(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            // validate modelstate
+            if (!ModelState.IsValid) return Page();
+
+            // validate email address (optional since we already configured it in the options in Program.cs)
+
+            // Create the user
+            // create the user object
+            var user = new IdentityUser
+            {
+                Email = RegisterViewModel.Email,
+                UserName = RegisterViewModel.Email
+            };
+
+            var result = await this.userManager.CreateAsync(user, RegisterViewModel.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Account/Login");
+            }
+            else
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("Register",error.Description);
+                }
+                return Page();
+            }
+        }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(dataType: DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
